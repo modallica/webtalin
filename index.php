2c13c92b6c1c0f87ad71fbc295ba4f2b52ca519c
modallica <42710768+modallica@users.noreply.github.com>
2025-10-03 03:11:36 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-// Silence is golden.
+get_header();
+?>
+<h1>Welcome to My Sample Theme</h1>
+<p>This theme is synced with GitHub using WP GitHub Theme Sync plugin.</p>
+<?php
+get_footer();
